0d1d601a3b17f5dd3427e0bcb4465fb14c4ab178
xuzhanchuan <xuzhanchuan@lansent.com>
2021-03-10 18:13:40 +0800

    修改登录页面
    登录页面调整,采用服务器登录.

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,80 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '晴川阁木s',
     userInfo: {},
+    loading: false,
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({
-      url: '../logs/logs'
+      //url: '../logs/logs'
+      url: '../resource/resource'
     })
   },
-  onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo(e) {
+  // onLoad() {
+  //   if (app.globalData.userInfo) {
+  //     console.log("userinfo is empty");
+  //     this.setData({
+  //       userInfo: app.globalData.userInfo,
+  //       hasUserInfo: true
+  //     })
+  //   } else if (this.data.canIUse) {
+  //     console.log("userinfo can't use");
+  //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+  //     // 所以此处加入 callback 以防止这种情况
+  //     // app.userInfoReadyCallback = res => {
+  //     //   console.log(res);
+  //     //   this.setData({
+  //     //     userInfo: res.userInfo,
+  //     //     hasUserInfo: true
+  //     //   })
+  //     // }
+  //     wx.login({
+  //       success (res) {
+  //         console.log(res);
+  //         if (res.code) {
+  //           //发起网络请求
+  //           wx.request({
+  //             url: 'https://api.qcgm.club/wechat/login',
+  //             data: {
+  //               code: res.code
+  //             }
+  //           })
+  //         } else {
+  //           console.log('登录失败！' + res.errMsg)
+  //         }
+  //       }
+  //     })
+  //   } else {
+  //     // 在没有 open-type=getUserInfo 版本的兼容处理
+  //     console.log(1111)
+  //     wx.getUserInfo({
+  //       success: res => {
+  //         app.globalData.userInfo = res.userInfo
+  //         this.setData({
+  //           userInfo: res.userInfo,
+  //           hasUserInfo: true
+  //         })
+  //       }
+  //     })
+
+      
+  //   }
+  // },
+  loginSubmit(e) {
     console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+    this.setData({loading: true})
+    const res = wx.request({
+      url: 'https://api.qcgm.club/service/auth/login',
+      data: e.detail.value,
+      method: 'POST'
     })
+    const {code, status} = res || {}
+    if(status === 200) {
+      this.setData({loading: false})
+    } 
   }
 })

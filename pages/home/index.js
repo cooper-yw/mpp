70a272577aef845aa33cf8203cad12445513d6b9
xuzhanchuan <xuzhanchuan@lansent.com>
2021-03-22 15:20:42 +0800

    初始化项目

--- pages/home/index.js
+++ pages/home/index.js
@@ -10,7 +10,7 @@ Page(auto.checkLogin({
     index: 0,
     userinfo: wx.getStorageSync('userInfo') || null,
     images: './images/mpp1.png',
-    avatar: './images/mpp3.png',
+    avatar: './images/mpp2.png',
     list: [{
       "text": "消息",
       "iconPath": "/pages/home/images/icon-png/message_no.png",
